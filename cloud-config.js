ef173f80d793d2529bee4d8525c4901a2a13b8d9
victork4314-sys <victork4314@gmail.com>
2026-07-17 01:55:54 +0200

    Use Figureloom in cloud account configuration

--- cloud-config.js
+++ cloud-config.js
@@ -2,5 +2,5 @@ window.SCICANVAS_CLOUD_CONFIG = window.SCICANVAS_CLOUD_CONFIG || {
   supabaseUrl: 'https://yzjqciycdbnpnndxvpgq.supabase.co',
   supabaseAnonKey: 'sb_publishable_hHhDcLXqCmhJSA80NX0gtA_2L1lW4O0',
   redirectUrl: `${window.location.origin}${window.location.pathname}`,
-  appName: 'SciCanvas'
+  appName: 'Figureloom'
 };
