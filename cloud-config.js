3525da723ceee64094c7b16b769fbded40f50796
victork4314-sys <victork4314@gmail.com>
2026-07-17 00:04:45 +0200

    Connect SciCanvas to the live Supabase project

--- cloud-config.js
+++ cloud-config.js
@@ -1,6 +1,6 @@
 window.SCICANVAS_CLOUD_CONFIG = window.SCICANVAS_CLOUD_CONFIG || {
-  supabaseUrl: '',
-  supabaseAnonKey: '',
+  supabaseUrl: 'https://yzjqciycdbnpnndxvpgq.supabase.co',
+  supabaseAnonKey: 'sb_publishable_hHhDcLXqCmhJSA80NX0gtA_2L1lW4O0',
   redirectUrl: `${window.location.origin}${window.location.pathname}`,
   appName: 'SciCanvas'
 };
