9cd0eb194a898306e83bf7bc6f9cf38fa7727829
victork4314-sys <victork4314@gmail.com>
2026-07-17 21:47:54 +0200

    Advance cache for finished collaboration

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const FIGURELOOM_BUILD_ID = "chunk-6-loomy-all-assets-20260717-v1";
+const FIGURELOOM_BUILD_ID = "chunk-7-collaboration-finish-20260717-v1";
 const FIGURELOOM_CACHE_PREFIX = "figureloom-app-";
 const FIGURELOOM_CACHE_NAME = `${FIGURELOOM_CACHE_PREFIX}${FIGURELOOM_BUILD_ID}`;
 const LEGACY_CACHE_PREFIXES = ["figureloom-shell", "scicanvas-shell"];
