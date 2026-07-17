05ae4ef755fa5aec1a9654a7469c76f54688ee73
victork4314-sys <victork4314@gmail.com>
2026-07-17 14:23:21 +0200

    Update cloud display name to FigureLoom

--- cloud-config.js
+++ cloud-config.js
@@ -2,5 +2,5 @@ window.SCICANVAS_CLOUD_CONFIG = window.SCICANVAS_CLOUD_CONFIG || {
   supabaseUrl: 'https://yzjqciycdbnpnndxvpgq.supabase.co',
   supabaseAnonKey: ['sb', 'publishable', 'hHhDcLXqCmhJSA80NX0gtA', '2L1lW4O0'].join('_'),
   redirectUrl: 'https://figureloom.org/',
-  appName: 'Figureloom'
+  appName: 'FigureLoom'
 };
