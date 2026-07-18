39f30fb40dc55a9a6612fe4bb16bd1afef56a6de
victork4314-sys <victork4314@gmail.com>
2026-07-18 13:58:08 +0200

    Use deterministic text layout bundle

--- safe-refresh.js
+++ safe-refresh.js
@@ -26,9 +26,7 @@
     "pptx-safe-jpeg-export.js",
     "code-window-tools.js",
     "code-window-startup-render.js",
-    "text-layout-protect-bounds.js",
-    "text-layout-tools.js",
-    "text-layout-new-text-default.js"
+    "text-layout-bundle.js"
   ];
   let reloading = false;
 
