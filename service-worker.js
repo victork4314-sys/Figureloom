747045a3d7538b0cd51e3f230d922af1998c473c
victork4314-sys <victork4314@gmail.com>
2026-07-18 14:54:55 +0200

    Bump cache for normal pasted text blocks

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const FIGURELOOM_BUILD_ID = "chunk-28-text-selection-resize-20260718-v1";
+const FIGURELOOM_BUILD_ID = "chunk-29-normal-pasted-text-blocks-20260718-v1";
 const FIGURELOOM_CACHE_PREFIX = "figureloom-app-";
 const FIGURELOOM_CACHE_NAME = `${FIGURELOOM_CACHE_PREFIX}${FIGURELOOM_BUILD_ID}`;
 const LEGACY_CACHE_PREFIXES = ["figureloom-shell", "scicanvas-shell"];
