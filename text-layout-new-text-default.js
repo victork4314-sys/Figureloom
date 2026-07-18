2c094893e11f805f02307ce810c2b6d47c0cd698
victork4314-sys <victork4314@gmail.com>
2026-07-18 14:53:03 +0200

    Create text as normal wrapped blocks

--- text-layout-new-text-default.js
+++ text-layout-new-text-default.js
@@ -9,8 +9,9 @@
     item.textAlign ??= 'left';
     item.textVerticalAlign ??= 'top';
     item.textPadding ??= 9;
-    item.lineHeight ??= 1.25;
-    item.width = 320;
+    item.lineHeight = 1.15;
+    item.width = 480;
+    item.textBoxWidth = 480;
     item.height = 62;
     render();
     scheduleSave();
