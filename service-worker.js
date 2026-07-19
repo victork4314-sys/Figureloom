f4a8c45dcad269c646b8b93bfbac75c93903f388
victork4314-sys <victork4314@gmail.com>
2026-07-19 04:21:57 +0200

    Invalidate older phone polish caches

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const FIGURELOOM_BUILD_ID = "stable-71d36df-locked-20260719-v32";
+const FIGURELOOM_BUILD_ID = "stable-71d36df-locked-20260719-v33";
 const FIGURELOOM_CACHE_PREFIX = "figureloom-app-";
 const FIGURELOOM_CACHE_NAME = `${FIGURELOOM_CACHE_PREFIX}${FIGURELOOM_BUILD_ID}`;
 const LEGACY_CACHE_PREFIXES = ["figureloom-shell", "scicanvas-shell"];
