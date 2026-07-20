5acfc5485fcd40ba4d2290e7c6764fa5a7a317d9
victork4314-sys <victork4314@gmail.com>
2026-07-20 20:45:59 +0200

    Bump stable desktop polish build to v77

--- safe-refresh.js
+++ safe-refresh.js
@@ -1,5 +1,6 @@
 (() => {
-  if (window.__figureLoomStableRuntime71d36dfV76) return;
+  if (window.__figureLoomStableRuntime71d36dfV77) return;
+  window.__figureLoomStableRuntime71d36dfV77 = true;
   window.__figureLoomStableRuntime71d36dfV76 = true;
   window.__figureLoomStableRuntime71d36dfV75 = true;
   window.__figureLoomStableRuntime71d36dfV74 = true;
@@ -40,7 +41,7 @@
   window.__figureLoomStableRuntime71d36dfV39 = true;
   window.__figureLoomStableRuntime71d36dfV38 = true;
 
-  const STABLE_BUILD = "stable-71d36df-locked-20260720-v76";
+  const STABLE_BUILD = "stable-71d36df-locked-20260720-v77";
   const GENERAL_ADDONS = [
     "library-more-illustrations.js",
     "library-more-templates.js",
