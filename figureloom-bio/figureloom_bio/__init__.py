c8064cfc745c5d68b057d33c22f41aa37dbbc744
victork4314-sys <victork4314@gmail.com>
2026-07-22 20:57:43 +0200

    Load the genomics core

--- figureloom-bio/figureloom_bio/__init__.py
+++ figureloom-bio/figureloom_bio/__init__.py
@@ -4,11 +4,13 @@ from .runtime import Runner
 from .runtime_extensions import install_runtime_extensions
 from .runtime_repeat_reset import install_repeat_reset
 from .sequence_management import install_sequence_management
+from .genomics_core import install_genomics_core
 
 install_runtime_extensions(Runner)
 install_repeat_reset(Runner)
 install_sequence_management(Runner)
+install_genomics_core(Runner)
 
-__version__ = "0.2.0"
+__version__ = "0.3.0"
 
 __all__ = ["Runner", "__version__"]
