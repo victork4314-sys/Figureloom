d943c97f0d636eafa6c19fdfab974b6fa5471ea5
victork4314-sys <victork4314@gmail.com>
2026-07-22 19:40:47 +0200

    Load the approved runtime extensions

--- figureloom-bio/figureloom_bio/__init__.py
+++ figureloom-bio/figureloom_bio/__init__.py
@@ -1,3 +1,10 @@
 """FigureLoom Bio."""
 
+from .runtime import Runner
+from .runtime_extensions import install_runtime_extensions
+
+install_runtime_extensions(Runner)
+
 __version__ = "0.1.0"
+
+__all__ = ["Runner", "__version__"]
