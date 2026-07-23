31b7745b61c361f27e81526c9371f8d83db92e96
victork4314-sys <victork4314@gmail.com>
2026-07-24 00:47:40 +0200

    Run the real automatic test in packaged self-tests

--- figureloom-bio/platform/test_entry.py
+++ figureloom-bio/platform/test_entry.py
@@ -6,10 +6,12 @@ if "--self-test" in sys.argv:
     os.environ.setdefault("QT_QPA_PLATFORM", "offscreen")
 
 from figureloom_bio import platform_qt_tools
+from figureloom_bio.final_platform_gaps import install_final_platform_gaps
 from figureloom_bio.platform_tool_safety import install_platform_tool_safety
 
 
 install_platform_tool_safety(platform_qt_tools)
+install_final_platform_gaps(platform_qt_tools)
 
 
 if __name__ == "__main__":
